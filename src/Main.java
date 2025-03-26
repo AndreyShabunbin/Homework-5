9fd048d290e19608d554e75c388f0ad211fc34a0
AndreyShabunin <schabuninandrew@yandex.ru>
2025-03-26 17:44:59 +0500

    Ошибку в задаче 3 исправил

--- src/Main.java
+++ src/Main.java
@@ -27,7 +27,9 @@ public class Main {
         if (year > 1584) {
             if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
                 System.out.println(year + " год является високосным");
-            } else System.out.println(year + " год не является високосным");
+            } else {
+                System.out.println(year + " год не является високосным");
+            }
         } else {
             System.out.println(year + " год не является допустимым (год должен быть больше 1584)");
         }
